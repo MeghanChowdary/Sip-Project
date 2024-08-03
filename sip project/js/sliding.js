4c36210c3bebae8881a97e878a66e4839bd14c01
MeghanChowdary <meghanchowdary6@gmail.com>
2024-08-03 11:56:09 +0530

    sample commit

---
+++ sip project/js/sliding.js
@@ -0,0 +1,210 @@
+const wrapper1 = document.querySelector(".amla");
+const attributes1 = document.querySelectorAll(".attribute");
+attributes1.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper1.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper2 = document.querySelector(".amla_murabba");
+const attributes2 = document.querySelectorAll(".attribute");
+attributes2.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper2.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper3 = document.querySelector(".ashwagandha");
+const attributes3 = document.querySelectorAll(".attribute");
+attributes3.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper3.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper4 = document.querySelector(".bibhitaki");
+const attributes4 = document.querySelectorAll(".attribute");
+attributes4.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper4.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper5 = document.querySelector(".bitter_melon");
+const attributes5 = document.querySelectorAll(".attribute");
+attributes5.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper5.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper6 = document.querySelector(".boosewellia");
+const attributes6 = document.querySelectorAll(".attribute");
+attributes6.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper6.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper7 = document.querySelector(".bramhi");
+const attributes7 = document.querySelectorAll(".attribute");
+attributes7.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper7.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper8 = document.querySelector(".cardamom");
+const attributes8 = document.querySelectorAll(".attribute");
+attributes8.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper8.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper9 = document.querySelector(".cumin");
+const attributes9 = document.querySelectorAll(".attribute");
+attributes9.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper9.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper10 = document.querySelector(".gotu_kola");
+const attributes10 = document.querySelectorAll(".attribute");
+attributes10.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper10.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper11 = document.querySelector(".haritaki");
+const attributes11 = document.querySelectorAll(".attribute");
+attributes11.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper11.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper12 = document.querySelector(".licorice");
+const attributes12 = document.querySelectorAll(".attribute");
+attributes12.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper12.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper13 = document.querySelector(".turmeric");
+const attributes13 = document.querySelectorAll(".attribute");
+attributes13.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper13.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper14 = document.querySelector(".garlic");
+const attributes14 = document.querySelectorAll(".attribute");
+attributes14.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper14.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper15 = document.querySelector(".ginger");
+const attributes15 = document.querySelectorAll(".attribute");
+attributes15.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper15.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper16 = document.querySelector(".summer_heat");
+const attributes16 = document.querySelectorAll(".attribute");
+attributes16.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper16.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper17 = document.querySelector(".sore_throat");
+const attributes17 = document.querySelectorAll(".attribute");
+attributes17.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper17.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper18 = document.querySelector(".nausea");
+const attributes18 = document.querySelectorAll(".attribute");
+attributes18.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper18.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper19 = document.querySelector(".dry_cough");
+const attributes19 = document.querySelectorAll(".attribute");
+attributes19.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper19.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper20 = document.querySelector(".headache");
+const attributes20 = document.querySelectorAll(".attribute");
+attributes20.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper20.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper21 = document.querySelector(".stomach_ache");
+const attributes21 = document.querySelectorAll(".attribute");
+attributes21.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper20.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper22 = document.querySelector(".common_cold");
+const attributes22 = document.querySelectorAll(".attribute");
+attributes22.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper22.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper23 = document.querySelector(".heatburn");
+const attributes23 = document.querySelectorAll(".attribute");
+attributes23.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper23.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper24 = document.querySelector(".gas");
+const attributes24 = document.querySelectorAll(".attribute");
+attributes24.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper24.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper25 = document.querySelector(".constipation");
+const attributes25 = document.querySelectorAll(".attribute");
+attributes25.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper25.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper26 = document.querySelector(".anemia");
+const attributes26 = document.querySelectorAll(".attribute");
+attributes26.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper26.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper27 = document.querySelector(".depression");
+const attributes27 = document.querySelectorAll(".attribute");
+attributes27.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper27.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper28 = document.querySelector(".allergy");
+const attributes28 = document.querySelectorAll(".attribute");
+attributes28.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper28.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper29 = document.querySelector(".motions");
+const attributes29 = document.querySelectorAll(".attribute");
+attributes29.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper29.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
+const wrapper30 = document.querySelector(".fever");
+const attributes30 = document.querySelectorAll(".attribute");
+attributes30.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper30.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
